5c30962dec33369057e0c825b05f22e767963754
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-13 11:29:58 -0500

    error displaying on no movie title

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,27 @@
 var searchFormEl = document.querySelector("#search-form");
 var movieInputEl = document.querySelector("#movie");
 
+
+// Get the modal
+var modal = document.getElementById("searchErrorModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// End of the modal
+
+
 var formSubmitHandler = function(event) {
   event.preventDefault();
   var movieSearch = movieInputEl.value.trim();
@@ -10,7 +31,8 @@ var formSubmitHandler = function(event) {
       getMovieData(movieSearch);
       movieInputEl.value = "";
   } else {
-      alert("Please enter a valid movie name!");
+      // alert("Please enter a valid movie name!");
+        modal.style.display = "block";
   }
   console.log(event);
 };
@@ -57,3 +79,5 @@ const getPlatforms = function (title) {
 getPlatforms("Titanic");
 
 searchFormEl.addEventListener('submit', formSubmitHandler)
+
+
